453c15a5c077ecafa30a7c147decb4731eaed3cf
Philip Newborough <corenominal@corenominal.org>
2016-06-10 15:46:20 +0100

    Add jQuery

--- functions/enqueue_scripts.php
+++ functions/enqueue_scripts.php
@@ -17,6 +17,14 @@ function corenominal_enqueue_scripts()
      * JavaScript
      */
     wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/vendor/modernizr-2.8.3.min.js', array(), false, false );
+    if ( !is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11 );
 
 }
 add_action( 'wp_enqueue_scripts', 'corenominal_enqueue_scripts' );
+
+function my_jquery_enqueue()
+{
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery-1.11.3.min.js', false, null, true );
+    wp_enqueue_script( 'jquery' );
+}
