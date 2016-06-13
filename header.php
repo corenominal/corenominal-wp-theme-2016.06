bc2490fd2a770e577e38214e0a252291d6f5f323
Philip Newborough <corenominal@corenominal.org>
2016-06-13 19:49:04 +0100

    A little bit of header cleaning

--- header.php
+++ header.php
@@ -5,16 +5,14 @@
 require get_template_directory() . '/meta.php';
 ?>
 <header class="masthead">
-<div id="logo" class="container">
 <?php if( is_home() || is_front_page() ): ?>
 <h1 class="site-title"><a href="<?php bloginfo('url') ?>" title="<?php bloginfo('description') ?>"><?php bloginfo('name') ?></a></h1>
 <?php else: ?>
 <h2 class="site-title"><a href="<?php bloginfo('url') ?>" title="<?php bloginfo('description') ?>"><?php bloginfo('name') ?></a></h2>
 <?php endif; ?>
+<?php //TODO: replace the below with a text widget ?>
 <p class="site-description"><strong><?php bloginfo('description'); ?></strong></p>
-<div id="nav-icons" class="nav-icons">
-<a class="search-icon" href="<?php echo site_url( 'search' ); ?>"><i class="fa fa-search"></i></a>
-</div>
+
 <nav id="site-menu" class="site-menu">
 <?php
 /**
@@ -22,7 +20,5 @@ require get_template_directory() . '/meta.php';
  */
 wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
 ?>
-
 </nav>
-</div>
 </header>
