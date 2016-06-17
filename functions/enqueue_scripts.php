bfa53a137c47c501ce2afb18d0e4fbea07330a5f
Philip Newborough <corenominal@corenominal.org>
2016-06-17 19:01:29 +0100

    Add some styles to get started

--- functions/enqueue_scripts.php
+++ functions/enqueue_scripts.php
@@ -10,6 +10,7 @@ function corenominal_enqueue_scripts()
      */
      wp_enqueue_style( 'corenominal_reset', get_template_directory_uri() . '/css/vendor/reset.css', false );
      wp_enqueue_style( 'corenominal_typography', get_template_directory_uri() . '/css/typography.css', false );
+     wp_enqueue_style( 'corenominal_style', get_template_directory_uri() . '/css/style.css', false );
 	 wp_enqueue_style( 'corenominal_layout', get_template_directory_uri() . '/css/layout.css', false );
 	 wp_enqueue_style( 'corenominal_shame', get_template_directory_uri() . '/css/shame.css', false );
 
