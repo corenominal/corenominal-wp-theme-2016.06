32a53fc4b3c1368c6f6d245c5392a606470f352a
Philip Newborough <corenominal@corenominal.org>
2016-06-20 21:00:11 +0100

    Import Share Tech Mono font

--- functions/enqueue_scripts.php
+++ functions/enqueue_scripts.php
@@ -9,6 +9,7 @@ function corenominal_enqueue_scripts()
      * CSS
      */
      wp_enqueue_style( 'corenominal_reset', get_template_directory_uri() . '/css/vendor/reset.css', false );
+     wp_enqueue_style( 'share_tech_mono_font', get_template_directory_uri() . '/fonts/Share-Tech-Mono-regular/fonts.css', false );
      wp_enqueue_style( 'corenominal_typography', get_template_directory_uri() . '/css/typography.css', false );
      wp_enqueue_style( 'corenominal_style', get_template_directory_uri() . '/css/style.css', false );
 	 wp_enqueue_style( 'corenominal_layout', get_template_directory_uri() . '/css/layout.css', false );
