52d2d27da31b4e7ff7d8f3686cc83b78a493052f
Philip Newborough <corenominal@corenominal.org>
2016-06-20 21:54:31 +0100

    Force jQuery into footer

--- functions/enqueue_scripts.php
+++ functions/enqueue_scripts.php
@@ -20,7 +20,8 @@ function corenominal_enqueue_scripts()
      */
     wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/vendor/modernizr-2.8.3.min.js', array(), false, false );
     wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/main.js', array(), false, true );
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
     wp_enqueue_script( 'jquery' );
-    //wp_enqueue_script( 'jquery_js', get_template_directory_uri() . '/js/main.js', array(), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'corenominal_enqueue_scripts' );
